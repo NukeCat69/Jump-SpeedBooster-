0c4d01bbabe312d3f3682134825dfb296597d941
NukeCat <61837881+NukeCat69@users.noreply.github.com>
2022-07-25 23:45:21 -0700

    Add files via upload

---
+++ booster.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class booster : MonoBehaviour
+{
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        switch (hit.gameObject.tag)
+        {
+            case "SpeedBoost":
+                speed = 25f;
+                break;
+            case "JumpPad":
+                jumpHeight = 25f;
+                break;
+            case "Ground":
+                jumpHeight = 3f;
+                speed = 12f;
+                break;
+            case "dead":
+                controller.enabled = false;
+                controller.transform.position = spawnpoint.transform.position;
+                controller.enabled = true;
+                break;
+
+        }
+    }
+
+}
